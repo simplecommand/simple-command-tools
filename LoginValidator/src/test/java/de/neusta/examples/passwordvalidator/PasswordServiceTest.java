febd055900a2d945e635e7692dbbb989b37e7ea3
mwolff <mwolff@laptopc3100>
2016-01-07 11:48:08 +0100

    Changed to mix configuration: Application and classpath

--- LoginValidator/src/test/java/de/neusta/examples/passwordvalidator/PasswordServiceTest.java
+++ LoginValidator/src/test/java/de/neusta/examples/passwordvalidator/PasswordServiceTest.java
@@ -31,7 +31,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = AppConfig.class)
+@ContextConfiguration("classpath:application.xml")
 public class PasswordServiceTest {
 
     @Resource
