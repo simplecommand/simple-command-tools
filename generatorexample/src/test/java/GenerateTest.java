9e9bd6e96969249cc48c107d81673320239869c1
Manfred Wolff <m.wolff@neusta.de>
2016-04-26 15:52:57 +0200

    Solved Bug in maven generator configuration

---
+++ generatorexample/src/test/java/GenerateTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import runner.SpringRunner;
+
+public class GenerateTest {
+
+    @Test
+    public void test() {
+        String[] arguments = new String[]{""};
+        SpringRunner.main(arguments);
+    }
+
+}
