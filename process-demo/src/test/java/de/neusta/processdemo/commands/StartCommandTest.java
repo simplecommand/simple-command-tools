cd6215afd18c97894b58452c6effc1882dac3a63
Manfred Wolff <m.wolff@neusta.de>
2016-04-29 15:27:22 +0200

    Renamed identifiers in xml to be closer to other frameworks.

--- process-demo/src/test/java/de/neusta/processdemo/commands/StartCommandTest.java
+++ process-demo/src/test/java/de/neusta/processdemo/commands/StartCommandTest.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import de.mwolff.commons.command.DefaultCommandContainer;
 import de.mwolff.commons.command.DefaultTransition;
-import de.mwolff.commons.command.iface.CommandContainer;
 import de.mwolff.commons.command.iface.Transition;
 import de.neusta.processdemo.parameterobject.ProcessParameter;
 
@@ -16,23 +14,26 @@ public class StartCommandTest {
     @Test
     public void isStartet() throws Exception {
         ProcessParameter processParameter = new ProcessParameter();
+        StartCommand<ProcessParameter> startCommand = createStartCommand();
+        processParameter.setStarted(false);
+        String next = startCommand.executeAsProcess("Start", processParameter);
+        assertThat(processParameter.isStarted(), is(true));
+        assertThat(next, is("OK"));
+        
+    }
 
+    private StartCommand<ProcessParameter> createStartCommand() {
         StartCommand<ProcessParameter> startCommand = new StartCommand<>();
         startCommand.setProcessID("Start");
+        Transition transition = createTransition();
+        startCommand.addTransition(transition);
+        return startCommand;
+    }
+
+    private Transition createTransition() {
         Transition transition = new DefaultTransition();
         transition.setReturnValue("OK");
         transition.setTarget("CustomerExists");
-        startCommand.addTransition(transition);
-        
-        processParameter.setStarted(false);
-
-        CommandContainer<ProcessParameter> commandContainer = new DefaultCommandContainer<>();
-        commandContainer.addCommand(startCommand);
-        
-        String next = commandContainer.executeAsProcess("Start", processParameter);
-        assertThat(processParameter.isStarted(), is(true));
-        
+        return transition;
     }
- 
-    
 }
