ce96d10473fb4f6a722d187c6678eeea691c2d60
Manfred Wolff <m.wolff@neustra.de>
2017-06-13 13:16:31 +0200

    Initial checkin with simple scanner implementation.

---
+++ bootstrap/src/main/java/bootstrap/PackageScanner.java
@@ -0,0 +1,30 @@
+package bootstrap;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+public class PackageScanner {
+
+    public Set<Class<?>> getSubTypesOf(String packagePath, Class<Object> classToFind) {
+        List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
+        classLoadersList.add(ClasspathHelper.contextClassLoader());
+        classLoadersList.add(ClasspathHelper.staticClassLoader());
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+            .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
+            .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+            .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packagePath))));
+    
+        Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+        return classes;
+    }
+
+}
---
+++ bootstrap/src/test/java/bootstrap/PackageScannerTest.java
@@ -0,0 +1,26 @@
+package bootstrap;
+
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PackageScannerTest {
+
+    @Test
+    public void testScanPackage() throws Exception {
+
+        PackageScanner packageScanner = new PackageScanner();
+        
+        Set<Class<?>> scannedClasses = packageScanner.getSubTypesOf("bootstrap", Object.class);
+
+        assertThat(scannedClasses.size(), is(2));
+        
+        for (Class<?> classEntry : scannedClasses) {
+            System.out.println(classEntry.getName());
+        }
+        
+    }
+    
+}
