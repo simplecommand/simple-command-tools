73ce3477944ac50c02ebbc87f0cfbc0e6fc92ce4
Manfred Wolff <m.wolff@neustra.de>
2017-08-10 11:30:45 +0200

    initial loguin

---
+++ command-archtype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.mwolff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
