c548bcdae2cf80765a3f3d72439f2935364fec4b
Manfred Wolff <wolff@manfred-wolff.de>
2017-08-31 10:01:59 +0200

    Released 1.4.0

--- command-archtype/create.sh
+++ command-archtype/create.sh
@@ -1,1 +1,1 @@
-mvn archetype:generate -DarchetypeGroupId=org.mwolff -DarchetypeArtifactId=command-archetype -DarchetypeVersion=1.3.1 -DgroupId=<your-groupId> -DartifactId=<your-artefactId>
+mvn archetype:generate -DarchetypeGroupId=org.mwolff -DarchetypeArtifactId=command-archetype -DarchetypeVersion=1.4.0 -DgroupId=<your-groupId> -DartifactId=<your-artefactId>
