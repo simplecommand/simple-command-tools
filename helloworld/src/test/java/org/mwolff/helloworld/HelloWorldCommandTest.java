2a3919f3c84207c5f22dc34bdd4fd06414fe3e24
Manfred Wolff <wolff@manfred-wolff.de>
2017-09-18 22:06:06 +0200

    Some approvement

--- helloworld/src/test/java/org/mwolff/helloworld/HelloWorldCommandTest.java
+++ helloworld/src/test/java/org/mwolff/helloworld/HelloWorldCommandTest.java
@@ -3,30 +3,27 @@ package org.mwolff.helloworld;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.mwolff.command.CommandTransitionEnum.CommandTransition;
 import org.mwolff.command.parameterobject.DefaultParameterObject;
 import org.mwolff.command.parameterobject.GenericParameterObject;
 
 public class HelloWorldCommandTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Test
-    public void testExecute() throws Exception {
-        thrown.expect(UnsupportedOperationException.class);
-        HelloWorldCommand.getInstance().execute(null);
-    }
-    
+    /**
+     * Tests if getInstance() returns unique instances and the right interface.
+     */
     @Test
     public void getInstanceTest() throws Exception {
-        HelloWorldCommand instance = HelloWorldCommand.getInstance();
-        assertThat(instance, instanceOf(HelloWorldCommand.class));
+        HelloWorldCommand hello = HelloWorldCommand.getInstance();
+        HelloWorldCommand world = HelloWorldCommand.getInstance();
+        assertThat(HelloWorldCommand.getInstance(), instanceOf(HelloWorldCommand.class));
+        assertThat(hello, is(not(world)));
     }
-    
+
+    /**
+     * Tests if the command sets a token named helloworld.message with the right value. 
+     */
     @Test
     public void testExecuteHelloWorld() throws Exception {
         GenericParameterObject context = DefaultParameterObject.getInstance();
