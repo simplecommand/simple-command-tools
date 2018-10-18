f1ee60f9ccced3dfffbf50315f4fc51c2b50f130
Manfred Wolff <wolff@manfred-wolff.de>
2018-10-18 09:53:02 +0200

    Migration auf Spring 4.3.20

--- toolbox/src/test/java/org/mwolff/toolbox/configuration/AppConfigTest.java
+++ toolbox/src/test/java/org/mwolff/toolbox/configuration/AppConfigTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "classpath:application.xml" })
+@ContextConfiguration({"classpath:application.xml"})
 public class AppConfigTest {
 
     @Resource
